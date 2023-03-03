b20da4f142a258a7842db58c34654a631659b09b
john_ <john_owen@eml.cc>
2023-03-03 17:02:22 +0000

    added Async Areas and Cards

--- async_examples.py
+++ async_examples.py
@@ -2,7 +2,7 @@ import logging, sys
 
 # Force to use pyGuardPoint from pyGuardPoint_Build directory
 sys.path.insert(1, 'pyGuardPoint_Build')
-from pyGuardPoint_Build.pyGuardPoint import GuardPointAsync, GuardPointError, Cardholder, Area
+from pyGuardPoint_Build.pyGuardPoint import GuardPointAsync, GuardPointError, Cardholder, Area, Card
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
@@ -28,18 +28,24 @@ if __name__ == "__main__":
                     print("\tLastname: " + cardholder.lastName)
                 if isinstance(entry, Area):
                     area = entry
-                    print("Cardholder:")
-                    print("\tArea UID: " + area.uid)
-                    print("\tArea Name: " + area.name)
+                    print("Area:")
+                    print("\tUID: " + area.uid)
+                    print("\tName: " + area.name)
+                if isinstance(entry, Card):
+                    card = entry
+                    print("Card:")
+                    print("\tCode: " + card.cardCode)
+                    print("\tType: " + card.cardType)
 
     try:
-        '''gp.get_card_holder(task_complete, uid="422edea0-589d-4224-af0d-77ed8a97ca57")
+        gp.get_card_holder(task_complete, uid="422edea0-589d-4224-af0d-77ed8a97ca57")
         gp.get_card_holder(task_complete, card_code="1B1A1B1C")
         gp.get_card_holders(task_complete, search_terms="john")
         gp.get_card_holders(task_complete, search_terms="robert")
         gp.get_card_holders(task_complete, search_terms="josh")
-        gp.get_card_holders(task_complete, search_terms="frida")'''
+        gp.get_card_holders(task_complete, search_terms="frida")
         gp.get_areas(task_complete)
+        gp.get_cards(task_complete)
     except Exception as e:
         print(e)
 
--- pyGuardPoint_Build/pyGuardPoint/guardpoint_async.py
+++ pyGuardPoint_Build/pyGuardPoint/guardpoint_async.py
@@ -10,44 +10,43 @@ class GuardPointAsync:
         self.executor = ThreadPoolExecutor(max_workers=1)
 
     def get_card_holder(self, on_finished, uid=None, card_code=None):
-        def handle_future(future):
-            try:
-                r = future.result()
-                on_finished(r)
-            except GuardPointError as e:
-                on_finished(e)
-            except Exception as e:
-                on_finished(GuardPointError(e))
-
         future = self.executor.submit(self.gp.get_card_holder, uid=uid, card_code=card_code)
-        future.add_done_callback(handle_future)
+        callback = GPAsyncCallBack(on_finished)
+        future.add_done_callback(callback.handle_future)
 
     def get_card_holders(self, on_finished, offset=0, limit=10, search_terms=None, cardholder_type_name=None):
-        def handle_future(future):
-            try:
-                r = future.result()
-                on_finished(r)
-            except GuardPointError as e:
-                on_finished(e)
-            except Exception as e:
-                on_finished(GuardPointError(e))
-
         future = self.executor.submit(self.gp.get_card_holders, offset=offset, limit=limit, search_terms=search_terms,
                                       cardholder_type_name=cardholder_type_name)
-        future.add_done_callback(handle_future)
+        callback = GPAsyncCallBack(on_finished)
+        future.add_done_callback(callback.handle_future)
+
+    def get_cards(self, on_finished):
+        future = self.executor.submit(self.gp.get_cards)
+        callback = GPAsyncCallBack(on_finished)
+        future.add_done_callback(callback.handle_future)
+
+    def delete_card(self, on_finished, card):
+        future = self.executor.submit(self.gp.delete_card)
+        callback = GPAsyncCallBack(on_finished)
+        future.add_done_callback(callback.handle_future, card)
 
     def get_areas(self, on_finished):
-        def handle_future(future):
-            try:
-                r = future.result()
-                on_finished(r)
-            except GuardPointError as e:
-                on_finished(e)
-            except Exception as e:
-                on_finished(GuardPointError(e))
-
         future = self.executor.submit(self.gp.get_areas)
-        future.add_done_callback(handle_future)
+        callback = GPAsyncCallBack(on_finished)
+        future.add_done_callback(callback.handle_future)
 
 
+class GPAsyncCallBack:
+    on_finished = None
 
+    def __init__(self, on_finished_func):
+        self.on_finished = on_finished_func
+
+    def handle_future(self, future):
+        try:
+            r = future.result()
+            self.on_finished(r)
+        except GuardPointError as e:
+            self.on_finished(e)
+        except Exception as e:
+            self.on_finished(GuardPointError(e))
