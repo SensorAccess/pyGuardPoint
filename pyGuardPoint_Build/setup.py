8e6c491009b4ab6bb9a3eb25cda211911d2295bd
john_ <john_owen@eml.cc>
2023-03-21 16:07:19 +0000

    5.5

--- pyGuardPoint_Build/setup.py
+++ pyGuardPoint_Build/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="pyGuardPoint",
-      version="0.5.4",
+      version="0.5.5",
       description="Easy-to-use Python module implementing GuardPoint's WebAPI",
       maintainer_email="sales@sensoraccess.co.uk",
       install_requires=['validators', 'fuzzywuzzy', 'Levenshtein'],
