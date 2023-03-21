ddcf85322b6e4b27af733f0506fa8eb019c01354
john_ <john_owen@eml.cc>
2023-03-21 10:31:38 +0000

    100 fuzz wuzz

--- examples/pyGP_tk_example_2.py
+++ examples/pyGP_tk_example_2.py
@@ -59,10 +59,13 @@ class App(tk.Tk):
         self.txtLookup = ttk.Entry(self.frmLookup, foreground="blue", background="white", width=30)
         self.btnLookup = ttk.Button(self.frmLookup, text="Lookup Cardholder", command=self.lookup_start)
         self.chkLookup_fuzzy = ttk.Checkbutton(self.frmLookup, text="Fuzzy search", variable=self.var_Lookup_isFuzzy)
-        self.sclLookup_threshold = ttk.Scale(self.frmLookup, variable=self.var_Lookup_thresholdLvl, from_=0, to=100, state="disabled")
-        self.lblLookup_threshold = ttk.Label(self.frmLookup, relief=tk.SUNKEN, width=4, text="-", anchor=tk.CENTER, state="disabled")
-        self.chkLookup_expired = ttk.Checkbutton(self.frmLookup, text="Include expired", variable=self.var_Lookup_isExpired)
-        self.lblLookup_result = ttk.Label(self.frmLookup, text=" - ", font=("Arial", 16), relief="sunken",)
+        self.sclLookup_threshold = ttk.Scale(self.frmLookup, variable=self.var_Lookup_thresholdLvl, from_=0, to=100,
+                                             state="disabled")
+        self.lblLookup_threshold = ttk.Label(self.frmLookup, relief=tk.SUNKEN, width=4, text="-", anchor=tk.CENTER,
+                                             state="disabled")
+        self.chkLookup_expired = ttk.Checkbutton(self.frmLookup, text="Include expired",
+                                                 variable=self.var_Lookup_isExpired)
+        self.lblLookup_result = ttk.Label(self.frmLookup, text=" - ", font=("Arial", 16), relief="sunken", )
 
         self.txtLookup.grid(row=1, column=1, columnspan=2, sticky="WE", padx=padding, pady=padding)
         self.btnLookup.grid(row=1, column=3, columnspan=2, sticky="WE", padx=padding, pady=padding)
@@ -74,13 +77,14 @@ class App(tk.Tk):
         self.frmLookup.columnconfigure(1, weight=1)
 
         self.var_Lookup_isFuzzy.trace_add("write", self._app_set_search)
-        self.var_Lookup_thresholdLvl.trace_add("write", lambda *_: self.lblLookup_threshold.config(text=str(self.var_Lookup_thresholdLvl.get())))
+        self.var_Lookup_thresholdLvl.trace_add("write", lambda *_: self.lblLookup_threshold.config(
+            text=str(self.var_Lookup_thresholdLvl.get())))
         self.var_Lookup_isExpired.set(True)
 
         # -------------------- Main Window -------------------- #
 
-        self.frmCHList.grid(row=1, column=1, sticky='NSWE', padx=padding*2, pady=padding*2)
-        self.frmLookup.grid(row=2, column=1, sticky='NSWE', padx=padding*2, pady=padding*2)
+        self.frmCHList.grid(row=1, column=1, sticky='NSWE', padx=padding * 2, pady=padding * 2)
+        self.frmLookup.grid(row=2, column=1, sticky='NSWE', padx=padding * 2, pady=padding * 2)
         self.columnconfigure(1, weight=1, pad=padding)
         self.rowconfigure(1, weight=1, pad=padding)
 
@@ -105,8 +109,10 @@ class App(tk.Tk):
         if isinstance(response, list):
             cardholders = response
             if len(cardholders) > 0:
-                self.lblLookup_result.config(text=f"Found: {cardholders[0].firstName} {cardholders[0].lastName}\n "
-                                                   f"within the {cardholders[0].insideArea.name}")
+                self.lblLookup_result.config(text=f"Found: {cardholders[0].firstName} {cardholders[0].lastName}\n"
+                                                  f"Company: {cardholders[0].cardholderPersonalDetail.company}\n"
+                                                  f"Email: {cardholders[0].cardholderPersonalDetail.email}\n"
+                                                  f"Area: {cardholders[0].insideArea.name}")
             else:
                 self.lblLookup_result.config(text=f"No Cardholder Found")
 
@@ -117,7 +123,7 @@ class App(tk.Tk):
             for area in self.area_list:
                 if self.lstCHList.get(i) == area.name:
                     area_filter_list.append(area)
-        filter_expired=(not self.var_Lookup_isExpired.get())
+        filter_expired = (not self.var_Lookup_isExpired.get())
         self.gp.get_card_holders(self.lookup_finished,
                                  search_terms=self.txtLookup.get(),
                                  areas=area_filter_list,
