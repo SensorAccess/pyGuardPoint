4666f5bce1303dfde5cf1bf9db510cb1c0e876a6
johnowen <john_owen@eml.cc>
2024-06-21 14:57:31 +0100

    version 1.5.3

--- get_cardholders_by_site.py
+++ get_cardholders_by_site.py
@@ -1,28 +1,45 @@
 import logging, sys
-
-sys.path.insert(1, 'pyGuardPoint_Build')
-from pyGuardPoint_Build.pyGuardPoint import GuardPoint, GuardPointError, GuardPointUnauthorized
+from importlib.metadata import version
+#sys.path.insert(1, 'pyGuardPoint_Build')
+#from pyGuardPoint_Build.pyGuardPoint import GuardPoint, GuardPointError, GuardPointUnauthorized
+from pyGuardPoint import GuardPoint, GuardPointError
 
 GP_HOST = 'https://sensoraccess.duckdns.org'
-GP_USER = 'admin'
-GP_PASS = 'admin'
+#GP_USER = 'admin'
+#GP_PASS = 'admin'
+#GP_USER = 'remko'
+#GP_PASS = 'remko'
+GP_USER = 'robert'
+GP_PASS = 'password'
 TLS_P12 = "/Users/johnowen/Downloads/MobileGuardDefault.p12"
 TLS_P12_PWD = "test"
+SITE_UID = "f342eade-eb43-4359-918d-d067d609fc38" # Sensor Office
+#SITE_UID = "90be9b9d-c87f-44b0-9ff9-e0f8725abbad" # Force site filter
+#SITE_UID = None
 
 if __name__ == "__main__":
+    py_gp_version = version("pyGuardPoint")
+    print("pyGuardPoint Version:" + py_gp_version)
+    py_gp_version_int = int(py_gp_version.replace('.', ''))
+    if py_gp_version_int < 153:
+        print("Please Update pyGuardPoint")
+        print("\t (Within a Terminal Window) Run > 'pip install pyGuardPoint --upgrade'")
+        exit()
+
     logging.basicConfig(level=logging.DEBUG)
     gp = GuardPoint(host=GP_HOST,
                     username=GP_USER,
                     pwd=GP_PASS,
                     p12_file=TLS_P12,
                     p12_pwd=TLS_P12_PWD,
-                    site_uid="11111111-1111-1111-1111-111111111111")
+                    site_uid=SITE_UID)
 
     try:
-        cardholder = gp.get_card_holder(uid="a7628540-0a5f-4c05-8347-59913c42d27e")
-        if cardholder is not None:
+        cardholders = gp.get_card_holders()
+        for cardholder in cardholders:
             print("Cardholder:")
-            print(f"\t{cardholder.uid}")
+            print(f"\townerSiteUID: {cardholder.ownerSiteUID}")
+            print(f"\tCardholderUID: {cardholder.uid}")
             print(f"\t{cardholder.firstName}")
             print(f"\t{cardholder.lastName}")
             print(f"\t{cardholder.cardholderPersonalDetail.email}")
